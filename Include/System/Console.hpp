c053a2b1e7ed76c72cbd8777703c2e2e171c2b04
Abolfazl Mohammadpour <abolfazlmohammadpourqaemshahr@gmail.com>
2022-08-02 01:44:15 -0700

    Add <WriteLine> Property In To <Include/System/Console.hpp>

--- Include/System/Console.hpp
+++ Include/System/Console.hpp
@@ -5,6 +5,10 @@ namespace System
     class Console
     {
     public: // Public Properties Of Console Class
-        static void WriteLine(std::string String);
+        // This Property Prints String Of Input Argument In New Line Of Console
+        static void WriteLine(std::string String)
+        {
+            std::cout << String << std::endl;
+        }
     };
 }
