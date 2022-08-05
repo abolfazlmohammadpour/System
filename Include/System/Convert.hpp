cb964412cabd8c28078119dfa93742b7a0de3d7f
Abolfazl Mohammadpour <abolfazlmohammadpourqaemshahr@gmail.com>
2022-08-05 11:55:02 -0700

    Add <ToLowerCase> And <ToUpperCase> In To <Include/System/Convert.hpp>

--- Include/System/Convert.hpp
+++ Include/System/Convert.hpp
@@ -5,5 +5,27 @@ namespace System
 {
     class Convert
     {
+    public: // Public Properties Of Convert Class
+        // This Property Convert UpperCase String To LowerCase String
+        static std::string ToLowerCase(std::string String)
+        {
+            for (unsigned short int Counter = 0; Counter < String.length(); Counter++)
+            {
+                String[Counter] = (char)tolower((int)String[Counter]);
+            }
+
+            return String;
+        }
+
+        // This Property Convert LowerCase String To UpperCase String
+        static std::string ToUpperCase(std::string String)
+        {
+            for (unsigned short int Counter = 0; Counter < String.length(); Counter++)
+            {
+                String[Counter] = (char)toupper((int)String[Counter]);
+            }
+
+            return String;
+        }
     };
 }
