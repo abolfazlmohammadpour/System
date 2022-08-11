2a3920b76b9c04d3aafdb85f08453f0f9ba17dea
Abolfazl Mohammadpour <abolfazlmohammadpourqaemshahr@gmail.com>
2022-08-11 06:53:56 -0700

    Add <ToBoolean> In To <Include/System/Convert.hpp>

--- Include/System/Convert.hpp
+++ Include/System/Convert.hpp
@@ -6,7 +6,7 @@ namespace System
     class Convert
     {
     public: // Public Properties Of Convert Class
-        // This Property Convert UpperCase String To LowerCase String
+        // This Property Convert A UpperCase String To LowerCase String
         static inline std::string ToLowerCase(std::string String)
         {
             for (unsigned short int Counter = 0; Counter < String.length(); Counter++)
@@ -17,7 +17,7 @@ namespace System
             return String;
         }
 
-        // This Property Convert LowerCase String To UpperCase String
+        // This Property Convert A LowerCase String To UpperCase String
         static inline std::string ToUpperCase(std::string String)
         {
             for (unsigned short int Counter = 0; Counter < String.length(); Counter++)
@@ -26,6 +26,170 @@ namespace System
             }
 
             return String;
+        }
+
+        // These Properties Convert A DataType To Boolean
+
+        // This Property Convert A Character To Boolean
+        static inline bool ToBoolean(char Character)
+        {
+            if (Character)
+                return true;
+            else
+                return false;
+        }
+
+        // This Property Convert A SignedCharacter To Boolean
+        static inline bool ToBoolean(signed char SignedChar)
+        {
+            if (SignedChar)
+                return true;
+            else
+                return false;
+        }
+
+        // This Property Convert A UnsignedChar To Boolean
+        static inline bool ToBoolean(unsigned char UnsignedChar)
+        {
+            if (UnsignedChar)
+                return true;
+            else
+                return false;
+        }
+
+        // This Property Convert A ShortInteger To Boolean
+        static inline bool ToBoolean(short int ShortInteger)
+        {
+            if (ShortInteger)
+                return true;
+            else
+                return false;
+        }
+
+        // This Property Convert A SignedShortInteger To Boolean
+        static inline bool ToBoolean(signed short int SignedShortInteger)
+        {
+            if (SignedShortInteger)
+                return true;
+            else
+                return false;
+        }
+
+        // This Property Convert A UnsignedShortInteger To Boolean
+        static inline bool ToBoolean(unsigned short int UnsignedShortInteger)
+        {
+            if (UnsignedShortInteger)
+                return true;
+            else
+                return false;
+        }
+
+        // This Property Convert A Integer To Boolean
+        static inline bool ToBoolean(int Integer)
+        {
+            if (Integer)
+                return true;
+            else
+                return false;
+        }
+
+        // This Property Convert A SignedInteger To Boolean
+        static inline bool ToBoolean(signed int SignedInteger)
+        {
+            if (SignedInteger)
+                return true;
+            else
+                return false;
+        }
+
+        // This Property Convert A UnsignedInteger To Boolean
+        static inline bool ToBoolean(unsigned int UnsignedInteger)
+        {
+            if (UnsignedInteger)
+                return true;
+            else
+                return false;
+        }
+
+        // This Property Convert A LongInteger To Boolean
+        static inline bool ToBoolean(long int LongInteger)
+        {
+            if (LongInteger)
+                return true;
+            else
+                return false;
+        }
+
+        // This Property Convert A SignedLongInteger To Boolean
+        static inline bool ToBoolean(signed long int SignedLongInteger)
+        {
+            if (SignedLongInteger)
+                return true;
+            else
+                return false;
+        }
+
+        // This Property Convert A UnsignedLongInteger To Boolean
+        static inline bool ToBoolean(unsigned long int UnsignedLongInteger)
+        {
+            if (UnsignedLongInteger)
+                return true;
+            else
+                return false;
+        }
+
+        // This Property Convert A LongLongInteger To Boolean
+        static inline bool ToBoolean(long long int LongLongInteger)
+        {
+            if (LongLongInteger)
+                return true;
+            else
+                return false;
+        }
+
+        // This Property Convert A SignedLongLongInteger To Boolean
+        static inline bool ToBoolean(signed long long int SignedLongLongInteger)
+        {
+            if (SignedLongLongInteger)
+                return true;
+            else
+                return false;
+        }
+
+        // This Property Convert A UnsignedLongLongInteger To Boolean
+        static inline bool ToBoolean(unsigned long long int UnsignedLongLongInteger)
+        {
+            if (UnsignedLongLongInteger)
+                return true;
+            else
+                return false;
+        }
+
+        // This Property Convert A FloatingPointInteger To Boolean
+        static inline bool ToBoolean(float FloatingPointInteger)
+        {
+            if (FloatingPointInteger)
+                return true;
+            else
+                return false;
+        }
+
+        // This Property Convert A DoubleFloatingPointInteger To Boolean
+        static inline bool ToBoolean(double DoubleFloatingPointInteger)
+        {
+            if (DoubleFloatingPointInteger)
+                return true;
+            else
+                return false;
+        }
+
+        // This Property Convert A LongDoubleFloatingPoint To Boolean
+        static inline bool ToBoolean(long double LongDoubleFloatingPoint)
+        {
+            if (LongDoubleFloatingPoint)
+                return true;
+            else
+                return false;
         }
     };
 }
