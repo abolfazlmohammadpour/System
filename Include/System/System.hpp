1e81ee9e8f0af0dc2295a87d3947fa269249496e
ABOLFAZL MOHAMMADPOUR <abolfazlmohammadpourqaemshahr@gmail.com>
2022-09-03 00:29:10 -0700

    Edit <Include/System/System.hpp>

--- Include/System/System.hpp
+++ Include/System/System.hpp
@@ -1,8 +1,13 @@
+// Standard C Plus Plus Libraries
 #include <iostream>
+
+// Standard C Libraries In Standard C Plus Plus Libraries
 #include <cctype>
 #include <climits>
+#include <cstdlib>
 #include <cstdarg>
 
+// System Libraries
 #include "./Type.hpp"
 #include "./Convert.hpp"
 #include "./Array.hpp"
