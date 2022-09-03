8f711d0c4e487613a0cb635a3d544d622c615255
ABOLFAZL MOHAMMADPOUR <abolfazlmohammadpourqaemshahr@gmail.com>
2022-09-03 00:25:21 -0700

    Link <Include/System/Array.hpp> In To <Include/System/System.hpp>

--- Include/System/System.hpp
+++ Include/System/System.hpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <cctype>
 #include <climits>
+#include <cstdarg>
 
 #include "./Type.hpp"
 #include "./Convert.hpp"
+#include "./Array.hpp"
 #include "./Color.hpp"
 #include "./Console.hpp"
