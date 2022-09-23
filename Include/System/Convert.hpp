5311dc1cab07465c844e8168a637c1bb3e1feb81
ABOLFAZL MOHAMMADPOUR <abolfazlmohammadpourqaemshahr@gmail.com>
2022-09-23 09:01:05 -0700

    Add <ToBinary <SignedInteger>> And <ToBinary <UnsignedInteger>> Properties In To <Include/System/Convert.hpp>

--- Include/System/Convert.hpp
+++ Include/System/Convert.hpp
@@ -87,5 +87,198 @@ namespace System
 
             return Data;
         }
+
+        // This Property Convert A Decimal Signed Integer (-2147483647 <= X <= +2147483647) To Binary And Return It
+        static inline Type::String ToBinary(Type::SignedInteger DecimalNumber)
+        {
+            if (DecimalNumber < -2147483647 || DecimalNumber > 2147483647)
+            {
+                throw "Error : OverLimit Of Signed Integer Type";
+            }
+
+            Type::String Binary = "";
+
+            Type::UnsignedShortInteger *OverflowBit = new Type::UnsignedShortInteger(1);
+
+            if (DecimalNumber == 0)
+            {
+                Binary += Convert::ToString(0);
+
+                while (Binary.length() < 32)
+                {
+                    Binary += '0';
+                }
+            }
+
+            else if (DecimalNumber > 0)
+            {
+                while (true)
+                {
+                    if (((DecimalNumber / 2) != 0) && ((DecimalNumber / 2) != 1))
+                    {
+                        Binary += Convert::ToString(DecimalNumber % 2);
+
+                        DecimalNumber /= 2;
+                    }
+
+                    else
+                    {
+                        if ((DecimalNumber == 0) || (DecimalNumber) == 1)
+                        {
+                            Binary += Convert::ToString(DecimalNumber);
+
+                            break;
+                        }
+
+                        else
+                        {
+                            Binary += Convert::ToString(DecimalNumber % 2);
+                            Binary += Convert::ToString(DecimalNumber / 2);
+
+                            break;
+                        }
+                    }
+                }
+
+                while (Binary.length() < 32)
+                {
+                    Binary += '0';
+                }
+            }
+
+            else if (DecimalNumber < 0)
+            {
+                DecimalNumber *= -1; // Convert Signed Integer To Unsigned Integer
+                while (true)
+                {
+                    if (((DecimalNumber / 2) != 0) && ((DecimalNumber / 2) != 1))
+                    {
+                        Binary += Convert::ToString(DecimalNumber % 2);
+
+                        DecimalNumber /= 2;
+                    }
+
+                    else
+                    {
+                        if ((DecimalNumber == 0) || (DecimalNumber) == 1)
+                        {
+                            Binary += Convert::ToString(DecimalNumber);
+
+                            break;
+                        }
+
+                        else
+                        {
+                            Binary += Convert::ToString(DecimalNumber % 2);
+                            Binary += Convert::ToString(DecimalNumber / 2);
+
+                            break;
+                        }
+                    }
+                }
+
+                while (Binary.length() < 32)
+                {
+                    Binary += '0';
+                }
+
+                // Toggle All Bits Of Binary
+                for (Type::UnsignedShortInteger Counter = 0; Counter < Binary.length(); Counter++)
+                {
+                    if (Binary[Counter] == '0')
+                    {
+                        Binary[Counter] = '1';
+                    }
+                    else
+                    {
+                        Binary[Counter] = '0';
+                    }
+                }
+
+                // Add A Overflow Bit To Toggled Binary
+                for (Type::Integer Counter = 0; Counter < Binary.length(); Counter++)
+                {
+                    if (*OverflowBit == 1)
+                    {
+                        if (Binary[Counter] == '1')
+                        {
+                            Binary[Counter] = '0';
+                        }
+
+                        else
+                        {
+                            *OverflowBit = 0;
+
+                            Binary[Counter] = '1';
+                        }
+                    }
+                }
+            }
+
+            delete OverflowBit;
+
+            // Rverse All Bits Of Binary
+            return Convert::StringReverse(Binary);
+        }
+
+        // This Property Convert A Decimal Unsigned Integer (0 <= X <= 4294967295) To Binary And Return It
+        static inline Type::String ToBinary(Type::UnsignedInteger DecimalNumber)
+        {
+            if (DecimalNumber < (Type::UnsignedInteger)0 || DecimalNumber > (Type::UnsignedInteger)4294967295)
+            {
+                throw "Error : OverLimit Of Unsigned Integer Type";
+            }
+
+            Type::String Binary = "";
+
+            if (DecimalNumber == 0)
+            {
+                Binary += Convert::ToString(0);
+
+                while (Binary.length() < 32)
+                {
+                    Binary += '0';
+                }
+            }
+
+            else
+            {
+                while (true)
+                {
+                    if (((DecimalNumber / 2) != 0) && ((DecimalNumber / 2) != 1))
+                    {
+                        Binary += Convert::ToString(DecimalNumber % 2);
+
+                        DecimalNumber /= 2;
+                    }
+
+                    else
+                    {
+                        if ((DecimalNumber == 0) || (DecimalNumber) == 1)
+                        {
+                            Binary += Convert::ToString(DecimalNumber);
+
+                            break;
+                        }
+
+                        else
+                        {
+                            Binary += Convert::ToString(DecimalNumber % 2);
+                            Binary += Convert::ToString(DecimalNumber / 2);
+
+                            break;
+                        }
+                    }
+                }
+
+                while (Binary.length() < 32)
+                {
+                    Binary += '0';
+                }
+            }
+
+            // Rverse All Bits Of Binary
+            return Convert::StringReverse(Binary);
+        }
     };
 }
